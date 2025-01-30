22df9af5af46d04f69e3204b36339441d08cdbb7
poikilos <7557867+poikilos@users.noreply.github.com>
2025-01-30 09:21:25 -0500

    Remove lint.

--- bumptonormalmap.py
+++ bumptonormalmap.py
@@ -72,7 +72,7 @@ def normalize(vec: np.array) -> np.array:
 def main():
     parser = argparse.ArgumentParser(description='Convert bump/height map to normal map')
     parser.add_argument('path', type=str, help='path to the input image (the bump map, i.e. the height map)')
-    parser.add_argument('strength', type=float, help='strength of the normal map. results in smoother (strength -> 0) or sharper (strength -> \\infty) features')
+    parser.add_argument('strength', type=float, help='strength of the normal map. results in smoother (strength -> 0) or sharper (strength -> \infty) features')
     parser.add_argument('output_format', type=str, choices=["png", "exr"], help='output image format')
     args = parser.parse_args()
 
@@ -95,9 +95,9 @@ def bump_to_normal(path, strength=1.0, output_format="png"):
         # convert grayscale to rgb
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    dim = img.shape
-    height = dim[0]
-    width = dim[1]
+    # dim = img.shape
+    # height = dim[0]
+    # width = dim[1]
 
     if strength <= 0.0:
         print("strength has to be >0")
@@ -109,8 +109,10 @@ def bump_to_normal(path, strength=1.0, output_format="png"):
     delta = 0
     ddepth = cv2.CV_64F
 
-    grad_x = cv2.Sobel(img, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_REPLICATE)
-    grad_y = cv2.Sobel(img, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_REPLICATE)
+    grad_x = cv2.Sobel(img, ddepth, 1, 0, ksize=3, scale=scale, delta=delta,
+                       borderType=cv2.BORDER_REPLICATE)
+    grad_y = cv2.Sobel(img, ddepth, 0, 1, ksize=3, scale=scale, delta=delta,
+                       borderType=cv2.BORDER_REPLICATE)
     print("Sobel filters applied")
 
     dx = grad_x[:, :, 0]
