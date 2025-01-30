6168ad1ffeeefbda58708ae2690e302707c5ca10
poikilos <7557867+poikilos@users.noreply.github.com>
2025-01-30 09:25:27 -0500

    Change comments to a stanard Python docstring (sets __doc__). Make sure output_format is correct, otherwise raise ValueError.

--- bumptonormalmap.py
+++ bumptonormalmap.py
@@ -1,37 +1,38 @@
-##################################
-# BUMP to NORMAL MAP
-##################################
-# python bumptonormalmap.py <path to bump map> <strength> <output format>
-#
-# <path to bump map> : string -> path to the input image (the bump map, i.e. the height map)
-#
-# <strength> : float > 0 -> "strength" of the normal map
-#                           results in smoother (strength -> 0) or sharper (strength -> \infty) features
-#                           strength = 1 (recommended to start with)
-#                           strength = 2 (more defined features)
-#                           strength = 10 (really strong normal mapping effect...)
-#                           just experiment a little bit :)
-#
-# <output format> : string -> "png" or "exr"; output image format. Use "exr" for higher precision.
-##################################
-# Uses horizontal and vertical sobel filters (https://docs.opencv.org/3.4/d2/d2c/tutorial_sobel_derivatives.html)
-# to detect edges to determine the gradients in horizontal and vertical direction respectively.
-#
-# GX = [ -1 0 1] = [1]
-#      [ -2 0 2]   [2] * [-1 0 1]
-#      [ -1 0 1]   [1]
-#
-# GY = [ -1 -2 -1] = [-1]
-#      [  0  0  0]   [ 0] * [1 2 1]
-#      [  1  2  1]   [ 1]
-#
-# The sobel filter is separable which allows to compute two one-dimensional convolutions instead of one two-dimensional.
-#
-# The normals are computed from the gradients in horizontal (dx) and vertical direction (dy) as follows:
-# normal = normalize(vec3(dx, dy, 1.0 / strength))
-#
-# Note that the normals are transformed from [-1,1] space to [0,1] space (normal * vec3(0.5) + vec3(0.5)).
-# Remember to undo the transformation (vec3(2.0) * normal - vec3(1.0)) when reading the normals from the normal map.
+"""
+BUMP to NORMAL MAP
+------------------
+python bumptonormalmap.py <path to bump map> <strength> <output format>
+
+<path to bump map> : string -> path to the input image (the bump map, i.e. the height map)
+
+<strength> : float > 0 -> "strength" of the normal map
+                          results in smoother (strength -> 0) or sharper (strength -> \infty) features
+                          strength = 1 (recommended to start with)
+                          strength = 2 (more defined features)
+                          strength = 10 (really strong normal mapping effect...)
+                          just experiment a little bit :)
+
+<output format> : string -> "png" or "exr"; output image format. Use "exr" for higher precision.
+
+Uses horizontal and vertical sobel filters (https://docs.opencv.org/3.4/d2/d2c/tutorial_sobel_derivatives.html)
+to detect edges to determine the gradients in horizontal and vertical direction respectively.
+
+GX = [ -1 0 1] = [1]
+     [ -2 0 2]   [2] * [-1 0 1]
+     [ -1 0 1]   [1]
+
+GY = [ -1 -2 -1] = [-1]
+     [  0  0  0]   [ 0] * [1 2 1]
+     [  1  2  1]   [ 1]
+
+The sobel filter is separable which allows to compute two one-dimensional convolutions instead of one two-dimensional.
+
+The normals are computed from the gradients in horizontal (dx) and vertical direction (dy) as follows:
+normal = normalize(vec3(dx, dy, 1.0 / strength))
+
+Note that the normals are transformed from [-1,1] space to [0,1] space (normal * vec3(0.5) + vec3(0.5)).
+Remember to undo the transformation (vec3(2.0) * normal - vec3(1.0)) when reading the normals from the normal map.
+"""
 import argparse
 import platform
 import sys
@@ -69,11 +70,14 @@ def normalize(vec: np.array) -> np.array:
     return vec / length
 
 
+NORMAL_FORMAT_CHOICES = ["png", "exr"]
+
+
 def main():
     parser = argparse.ArgumentParser(description='Convert bump/height map to normal map')
     parser.add_argument('path', type=str, help='path to the input image (the bump map, i.e. the height map)')
     parser.add_argument('strength', type=float, help='strength of the normal map. results in smoother (strength -> 0) or sharper (strength -> \infty) features')
-    parser.add_argument('output_format', type=str, choices=["png", "exr"], help='output image format')
+    parser.add_argument('output_format', type=str, choices=NORMAL_FORMAT_CHOICES, help='output image format')
     args = parser.parse_args()
 
     bump_to_normal(args.path, strength=args.strength,
@@ -124,8 +128,12 @@ def bump_to_normal(path, strength=1.0, output_format="png"):
     if output_format == "png":
         colors = np.uint8(colors * 255)
         cv2.imwrite("normal_map.png", colors)
-    else:
+    elif output_format == "exr":
         cv2.imwrite("normal_map.exr", colors.astype(np.float32))
+    else:
+        raise ValueError(
+            "Invalid format {} (expected {})"
+            .format(output_format, NORMAL_FORMAT_CHOICES))
 
     end = time.time()
     print("Finished. Conversion took " + str(end - start) + "s.")
